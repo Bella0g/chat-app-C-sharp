b71a440e63367a574078b4af184ea644b6923a13
Isabelle Gustafsson <isabellegustafsson@Isabelles-MacBook-Air.local>
2024-01-17 11:32:57 +0100

    Added messages that corresponds to keybindings

--- klient/Program.cs
+++ klient/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace chat_client;
 
-class ChatClient
+class ChatMessages
 {
     static void Main(string[] args)
     {
@@ -11,11 +11,13 @@ class ChatClient
         {
             if (isLoggedIn)
             {
+
                 // Användaren är inloggad, implementera keybindings för chattfunktioner
-                Console.WriteLine("1. Tryck l för att logga in.");
-                Console.WriteLine("2. Tryck i för att börja skriva meddelanden");
-                Console.WriteLine("3. Tryck Enter för att skicka.");
-                Console.WriteLine("4. Tryck Backspace för att gå tillbaka till login menyn.");
+                Console.WriteLine("Tryck r för att registrera användare.");
+                Console.WriteLine("Tryck l för att logga in.");
+                Console.WriteLine("Tryck i för att börja skriva meddelanden.");
+                Console.WriteLine("Tryck Enter för att skicka meddelandet.");
+                Console.WriteLine("Tryck Backspace för att gå tillbaka till registrering/login menyn.");
 
                 if (isInMessageMode)
                 {
@@ -27,7 +29,31 @@ class ChatClient
                 }
 
                 ConsoleKeyInfo key = Console.ReadKey();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("\nSkriv meddelande och tryck Enter:");
+                        string message = Console.ReadLine()!;
+                        // Skicka meddelandet till servern
+                        break;
+
+                        
+                    case ConsoleKey.D2:
+                        Console.WriteLine("\nAnge användarnamn för privat meddelande:");
+                        string recipient = Console.ReadLine();
+                        Console.WriteLine($"Skriv ditt privat meddelande till {recipient} och tryck Enter:");
+                        string privateMessage = Console.ReadLine();
+                        // Skicka det privata meddelandet till servern
+                        break;
+                }
+
             }
         }
     }
+}
+
+class ChatClient
+{
+
 }

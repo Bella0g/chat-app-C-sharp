aa1694bccb6d78091fe0dc580cdc433a72762672
Isabelle Gustafsson <isabellegustafsson@Isabelles-MacBook-Air.local>
2024-01-17 11:00:10 +0100

    Added class ChatClient

--- klient/Program.cs
+++ klient/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace chat_app_c_;
+﻿namespace chat_client;
 
-class Program
+class ChatClient
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        bool isLoggedIn = false;
+        bool isInMessageMode = false;
+
+        while (true)
+        {
+            if (isLoggedIn)
+            {
+                // Användaren är inloggad, implementera keybindings för chattfunktioner
+                Console.WriteLine("1. Tryck l för att logga in.");
+                Console.WriteLine("2. Tryck i för att börja skriva meddelanden");
+                Console.WriteLine("3. Tryck Enter för att skicka.");
+                Console.WriteLine("4. Tryck Backspace för att gå tillbaka till login menyn.");
+
+                if (isInMessageMode)
+                {
+                    Console.WriteLine("Tryck Enter för att skicka meddelandet");
+                }
+                else
+                {
+                    Console.WriteLine("Tryck i för att börja skriva meddelanden");
+                }
+
+                ConsoleKeyInfo key = Console.ReadKey();
+            }
+        }
     }
 }

9214a356a522fe4118882bda4e0d2149c8ff6de2
Isabelle Gustafsson <isabellegustafsson@Isabelles-MacBook-Air.local>
2024-01-17 15:11:38 +0100

    edited conflicting files

--- klient/Program.cs
+++ klient/Program.cs
@@ -1,87 +1,86 @@
-﻿namespace chat_client;
-
-class ChatMessages
+﻿namespace chat_client
 {
-    static void Main(string[] args)
+    class ChatMessages
     {
-        bool isLoggedIn = false;
-        bool isInMessageMode = false;
-
-        while (true)
+        static void Main(string[] args)
         {
-            if (isLoggedIn)
+            bool isLoggedIn = false;
+            bool isInMessageMode = false;
+
+            while (true)
             {
-                // Användaren är inloggad, implementera keybindings för chattfunktioner
-                Console.WriteLine("Tryck i för att börja skriva meddelanden.");
-                Console.WriteLine("Tryck Enter för att skicka meddelandet.");
-                Console.WriteLine("Tryck l för att logga ut.");
-
-                if (isInMessageMode)
+                if (isLoggedIn)
                 {
-                    Console.WriteLine("Tryck Enter för att skicka meddelandet");
+                    // Användaren är inloggad, implementera keybindings för chattfunktioner
+                    Console.WriteLine("Tryck i för att börja skriva meddelanden.");
+                    Console.WriteLine("Tryck Enter för att skicka meddelandet.");
+                    Console.WriteLine("Tryck l för att logga ut.");
+
+                    if (isInMessageMode)
+                    {
+                        Console.WriteLine("Tryck Enter för att skicka meddelandet");
+                    }
+
+                    ConsoleKeyInfo key = Console.ReadKey();
+
+                    switch (char.ToLower(key.KeyChar))
+                    {
+                        case 'i':
+                            isInMessageMode = true;
+                            break;
+
+                        case '\r': // '\r' represents the Enter key
+                            if (isInMessageMode)
+                            {
+                                Console.WriteLine("\nSkriv ditt meddelande:");
+                                string message = Console.ReadLine()!;
+                                // Skicka meddelandet till servern
+                                isInMessageMode = false;
+                            }
+                            break;
+
+                        case 'l':
+                            isLoggedIn = false;
+                            Console.WriteLine("\nDu har loggat ut.");
+                            break;
+
+                        default:
+                            Console.WriteLine("\nOgiltig tangent. Försök igen.");
+                            break;
+                    }
                 }
 
-                ConsoleKeyInfo key = Console.ReadKey();
-
-                switch (key.Key)
+                else
                 {
-                    case ConsoleKey.I:
-                        isInMessageMode = true;
-                        break;
+                    // Användaren är inte inloggad, implementera keybindings för inloggning och registrering
+                    Console.WriteLine("Tryck r för att registrera användare.");
+                    Console.WriteLine("Tryck l för att logga in.");
+                    Console.WriteLine("Tryck q för att avsluta programmet.");
 
-                    case ConsoleKey.Enter:
-                        if (isInMessageMode)
-                        {
-                            Console.WriteLine("\nSkriv ditt meddelande:");
-                            string message = Console.ReadLine()!;
-                            // Skicka meddelandet till servern
-                            isInMessageMode = false;
-                        }
-                        break;
+                    ConsoleKeyInfo key = Console.ReadKey();
 
-                    case ConsoleKey.L:
-                        isLoggedIn = false;
-                        Console.WriteLine("\nDu har loggat ut.");
-                        break;
+                    switch (char.ToLower(key.KeyChar))
+                    {
+                        case 'r':
+                            Console.WriteLine("\nAnge ditt användarnamn och lösenord för registrering:");
+                            // Registreringslogik
+                            break;
 
-                    default:
-                        Console.WriteLine("\nOgiltig tangent. Försök igen.");
-                        break;
+                        case 'l':
+                            Console.WriteLine("\nAnge ditt användarnamn och lösenord för inloggning:");
+                            // Inloggningslogik och sätt isLoggedIn till true om inloggningen är framgångsrik
+                            isLoggedIn = true;
+                            break;
+
+                        case 'q':
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+                            Console.WriteLine("\nOgiltig tangent. Försök igen.");
+                            break;
+                    }
                 }
-
-            }
-
-            else
-            {
-                // Användaren är inte inloggad, implementera keybindings för inloggning och registrering
-                Console.WriteLine("Tryck r för att registrera användare.");
-                Console.WriteLine("Tryck l för att logga in.");
-                Console.WriteLine("Tryck q för att avsluta programmet.");
-
-                ConsoleKeyInfo key = Console.ReadKey();
-
-                switch (key.Key)
-                {
-                    case ConsoleKey.R:
-                        Console.WriteLine("\nAnge ditt användarnamn och lösenord för registrering:");
-                        // Registreringslogik
-                        break;
-
-                    case ConsoleKey.L:
-                        Console.WriteLine("\nAnge ditt användarnamn och lösenord för inloggning:");
-                        // Inloggningslogik och sätt isLoggedIn till true om inloggningen är framgångsrik
-                        isLoggedIn = true;
-                        break;
-
-                    case ConsoleKey.Q:
-                        Environment.Exit(0);
-                        break;
-
-                    default:
-                        Console.WriteLine("\nOgiltig tangent. Försök igen.");
-                        break;
-                }
-
             }
         }
     }
